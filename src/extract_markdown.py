c435f06f9d924cbf39ed161142e0a43919631eae
ValentinoFilipetto <valefilipetto@gmail.com>
2024-12-26 11:08:35 +0100

    feat: add markdown block divider

--- src/extract_markdown.py
+++ src/extract_markdown.py
@@ -7,3 +7,29 @@ def extract_markdown_images(text):
 
 def extract_markdown_links(text):
     return re.findall(r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
+
+
+def markdown_to_blocks(text):
+    splitted_text = text.split("\n\n")
+    blocks = []
+
+    for block in splitted_text:
+        if block == "":
+            continue
+        else:
+            blocks.append(block.strip())
+
+    return blocks
+
+
+# def extract_title(markdown):
+#     array_of_lines = markdown.split("\n")
+#     header = None
+
+#     if array_of_lines[0].startswith("# ", 0, 2):
+#         return array_of_lines[0].strip("# ")
+
+#     if header == None:
+#         raise Exception("No header found in document")
+
+#     return header
--- src/test_extractmarkdown.py
+++ src/test_extractmarkdown.py
@@ -23,6 +23,70 @@ class TestExtractMarkdown(unittest.TestCase):
             ],
         )
 
+    def test_markdown_to_blocks(self):
+        text = """
+        # This is a heading
+
+        This is a paragraph of text. It has some **bold** and *italic* words inside of it.
+
+        * This is the first list item in a list block
+        * This is a list item
+        * This is another list item"""
+
+        self.assertEqual(
+            markdown_to_blocks(text),
+            [
+                "# This is a heading",
+                "This is a paragraph of text. It has some **bold** and *italic* words inside of it.",
+                "* This is the first list item in a list block\n        * This is a list item\n        * This is another list item",
+            ],
+        )
+
+    def test_markdown_to_blocks_excess_empty_lines(self):
+        text = """
+        # This is a heading
+
+
+
+
+        This is a paragraph of text. It has some **bold** and *italic* words inside of it.
+
+        * This is the first list item in a list block
+        * This is a list item
+        * This is another list item"""
+
+        self.assertEqual(
+            markdown_to_blocks(text),
+            [
+                "# This is a heading",
+                "This is a paragraph of text. It has some **bold** and *italic* words inside of it.",
+                "* This is the first list item in a list block\n        * This is a list item\n        * This is another list item",
+            ],
+        )
+
+    # def test_extract_title(self):
+    #     text = """# Lorem ipsum dolor sit amet,
+    #             consectetur adipiscing elit,
+    #             sed do eiusmod tempor incididunt
+    #             ut labore et dolore magna aliqua."""
+    #     self.assertEqual(extract_title(text), "Lorem ipsum dolor sit amet,")
+
+    # def test_extract_title_with_subheader(self):
+    #     text = """# Lorem ipsum dolor sit amet,
+    #             consectetur adipiscing elit,
+    #             ## sed do eiusmod tempor incididunt
+    #             ut labore et dolore magna aliqua."""
+    #     self.assertEqual(extract_title(text), "Lorem ipsum dolor sit amet,")
+
+    # def test_missing_header(self):
+    #     text = """## Lorem ipsum dolor sit amet,
+    #             consectetur adipiscing elit,
+    #             sed do eiusmod tempor incididunt
+    #             ut labore et dolore magna aliqua."""
+
+    #     with self.assertRaises(Exception):
+    #         extract_title(text)
+
 
 if __name__ == "__main__":
     unittest.main()
